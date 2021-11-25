646e19b9dd59acba332b07e9d2bd98f283b99a6b
christina bartholomæussen <c.m.bartholo@gmail.com>
2021-11-25 17:25:48 +0100

    deploy05

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
     res.send(frontpagePage);
 });
 
-const PORT = process.env.PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, (error) => {
     console.log("Server is running on port", PORT);
